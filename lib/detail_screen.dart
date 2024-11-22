ef60592cb944faac6aa070152287397642853cfb
Divarizky <divarizky28@gmail.com>
2024-11-22 14:50:50 +0700

    codelab-3
    menambahkan tampilan daftar pada main_screen.dart.
    
    menambahkan model yang berisikan informasi lokasi wisata di kota Bandung yaitu file tourism_place.dart

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,119 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:wisatabandung/model/tourism_place.dart';
 
-// variable yang digunakan untuk menyimpan TextSyle
-var informationTextStyle = const TextStyle(fontFamily: 'Nunito_Sans');
+var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final TourismPlace place;
+
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            //untuk memaksimalkan lebar ukuran dari Column
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset('images/farm-house.jpg'),
-              //menggunakan Container agar dapat memberikan property seperti margin atau padding pada isi konten
-              Container(
-                margin: const EdgeInsets.only(top: 16.0),
-                child: Text(
-                  'Farm House Lembang',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Nunito_Sans',
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Image.asset(place.imageAsset),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        const FavoriteButton(),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  // mengatur agar jaraknya berada di antara
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        const Icon(Icons.calendar_today),
-                        const SizedBox(height: 8.0),
-                        //menggunakan TextStyle yang sudah disimpan pada variable informationTextStyle
-                        Text('Open Everyday', style: informationTextStyle),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Icon(Icons.access_time),
-                        const SizedBox(height: 8.0),
-                        Text('09:00 - 20:00', style: informationTextStyle),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Icon(Icons.monetization_on),
-                        const SizedBox(height: 8.0),
-                        Text('Rp25,000', style: informationTextStyle),
-                      ],
-                    ),
-                  ],
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                place.name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Staatliches',
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                  textAlign: TextAlign.center,
-                  //menggunakan Custom Font yang ditambahkan pada file pubspec.yaml
-                  style: TextStyle(fontFamily: 'Oswald', fontSize: 16.0),
-                ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text(place.openDays, style: informationTextStyle),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.access_time),
+                      const SizedBox(height: 8.0),
+                      Text(place.openTime, style: informationTextStyle),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text(place.ticketPrice, style: informationTextStyle),
+                    ],
+                  ),
+                ],
               ),
-              // widget ListView dibungkus Sizedbox untu mengatur Tinggi dari setiap gambar menyesuaikan height yang kita inginkan
-              SizedBox(
-                height: 200,
-                //membungkus gambar dengan ListView agar tampilan dapat menggunakan Scrolling
-                child: ListView(
-                  scrollDirection:
-                      Axis.horizontal, //fungsi yang berfungsi untuk mengubah arah scroll pada isi dari ListView
-                  children: [
-                    //menambahkan widget Padding untuk mengatur jarak antara setiap gambarnya
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      //widget ClipRRect untuk memberikan property tambahan yaitu BorderRadius pada gambar
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        //Image.network untuk mengambil gambar dari link URL
-                        child: Image.network(
-                          'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                place.description,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children:
+                    place.imageUrls.map((url) {
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(url),
+                        ),
+                      );
+                    }).toList(),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
     );
   }
 }
